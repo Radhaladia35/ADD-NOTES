1e73dd13231dd1b2134800bb9d509646398fc697
Radhaladia35 <radhaladia35@gmail.com>
2024-11-14 12:43:46 +0530

    Add files via upload

---
+++ todo1.js
@@ -0,0 +1,28 @@
+const addBtn = document.querySelector("#addBtn")
+const main = document.querySelector("#main")
+addBtn.addEventListener('click', function () {
+   addNote()
+});
+const addNote = () => {
+   const note = document.createElement("div");
+   note.classList.add("note")
+   note.innerHTML =`<div class="toolbar"> 
+    <i class="userEdit far fa-edit"></i>
+        <i class="trash fa-solid fa-trash"></i>
+      </div>
+      <textarea></textarea>`
+ note.querySelector(".trash").addEventListener('click', function () {
+      note.remove()
+   });
+note.querySelector(".userEdit").addEventListener('click', function () {
+       if(note.lastChild.disabled){
+         note.lastChild.disabled=false;
+         note.lastChild.focus();
+}
+else{
+   note.lastChild.disabled=true;
+}})
+      
+main.appendChild(note);
+
+}
